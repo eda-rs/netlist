73d30327c5497a514d062cc762ae2b276943cca4
Eric <zhenyuxu163@163.com>
2021-05-15 20:18:25 +0800

    update

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -13,15 +13,21 @@ impl<W: Default, N: Default, G: Default, P: Default> NetList<W, N, G, P> {
         }
     }
 
-    // get internal net data by name
-    pub fn get_mut_net(&mut self, name: &str) -> NResult<&mut W> {
+    // update internal net data by name
+    pub fn update_net(&mut self, name: &str, new_data: &W) -> NResult<()>
+    where
+        W: Clone,
+    {
         match self.net_map.get(name) {
-            Some(i) => Ok(&mut self.nets[*i].data),
-            None => Err(Box::new(NetListError::NetNotFound(
-                name.to_string(),
-                self.name.clone(),
-            ))),
+            Some(i) => self.nets[*i].data = new_data.clone(),
+            None => {
+                return Err(Box::new(NetListError::NetNotFound(
+                    name.to_string(),
+                    self.name.clone(),
+                )))
+            }
         }
+        Ok(())
     }
 
     // get internal gate data by name
@@ -35,15 +41,21 @@ impl<W: Default, N: Default, G: Default, P: Default> NetList<W, N, G, P> {
         }
     }
 
-    // get mutable internal gate data by name
-    pub fn get_mut_gate(&mut self, name: &str) -> NResult<&mut G> {
+    // update internal gate data by name
+    pub fn update_gate(&mut self, name: &str, new_data: &G) -> NResult<()>
+    where
+        G: Clone,
+    {
         match self.gate_map.get(name) {
-            Some(i) => Ok(&mut self.gates[*i].data),
-            None => Err(Box::new(NetListError::GateNotFound(
-                name.to_string(),
-                self.name.clone(),
-            ))),
+            Some(i) => self.gates[*i].data = new_data.clone(),
+            None => {
+                return Err(Box::new(NetListError::GateNotFound(
+                    name.to_string(),
+                    self.name.clone(),
+                )))
+            }
         }
+        Ok(())
     }
 
     // get internal pin data by name
@@ -57,14 +69,20 @@ impl<W: Default, N: Default, G: Default, P: Default> NetList<W, N, G, P> {
         }
     }
 
-    // get mutable internal pin data by name
-    pub fn get_mut_pin(&mut self, name: &str) -> NResult<&mut P> {
+    // update internal pin data by name
+    pub fn get_mut_pin(&mut self, name: &str, new_data: &P) -> NResult<()>
+    where
+        P: Clone,
+    {
         match self.pin_map.get(name) {
-            Some(i) => Ok(&mut self.pins[*i].data),
-            None => Err(Box::new(NetListError::PinNotFound(
-                name.to_string(),
-                self.name.clone(),
-            ))),
+            Some(i) => self.pins[*i].data = new_data.clone(),
+            None => {
+                return Err(Box::new(NetListError::PinNotFound(
+                    name.to_string(),
+                    self.name.clone(),
+                )))
+            }
         }
+        Ok(())
     }
 }
